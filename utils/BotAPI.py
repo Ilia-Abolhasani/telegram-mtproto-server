8f1370183153c3bf1e2ffa2d7beff396e612b3e2
git.gtiz <milademadi74@gmail.com>
2023-08-14 10:29:23 +0330

    Add BotAPI send&edit message

---
+++ utils/BotAPI.py
@@ -0,0 +1,11 @@
+from pytgbot import Bot
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+API_KEY='6548756897:AAFuUVTDRc4bPLG44jOhYwcMAOlFLU-GJ4Y'
+CHAT='@myproxy786'
+bot = Bot(API_KEY)
+
+# res = bot.send_message(CHAT, "Example Text FROM BOT!")
+res = bot.edit_message_text("Example Text EDIT FROM BOT!", CHAT, 25)
+print(res.message_id)
