8ed32063d6b3a493867bef1102ade178230d7151
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-08-15 00:37:21 +0330

    bot api added to project

--- utils/BotAPI.py
+++ utils/BotAPI.py
@@ -1,12 +1,30 @@
+import os
 from pytgbot import Bot
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-API_KEY='6548756897:AAFuUVTDRc4bPLG44jOhYwcMAOlFLU-GJ4Y'
-CHAT='@myproxy786'
-bot = Bot(API_KEY)
 
-# res = bot.send_message(CHAT, "Example Text FROM BOT!")
-# res = bot.edit_message_text("Example Text EDIT FROM BOT!", CHAT, 25)
-bot.delete_message(CHAT,25)
-# print(res.message_id)
+class BotAPI:
+    def __init__(self, chat):
+        bot_api_key = os.getenv("bot_api_key")
+        self.bot = Bot(bot_api_key)
+        self.chat = chat
+
+    def send_message(self, text):
+        result = self.bot.send_message(self.chat, text)
+        return result
+
+    def edit_message_text(self, text, message_id):
+        result = self.bot.edit_message_text(text, self.chat, message_id)
+        return result
+
+    def delete_message(self, message_id):
+        result = self.bot.delete_message(self.chat, message_id)
+        return result
+
+    def pin_chat_message(self, message_id, disable_notification):
+        result = self.bot.pin_chat_message(
+            self.chat, message_id, disable_notification)
+        return result
+
+#
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .DotDict import DotDict
-from .Mtproto import extract_all_mtproto, is_valid_mtproto_link
+from .Mtproto import extract_all_mtproto, is_valid_mtproto_link, parse_proxy_link
 from .TelegramAPI import TelegramAPI
+from .BotAPI import BotAPI
