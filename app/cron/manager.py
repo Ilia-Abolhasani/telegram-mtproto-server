812c13afd040be2d7e688519ec75489d546b232d
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-08-26 00:25:03 +0330

    cron expresstion updated.

--- app/cron/manager.py
+++ app/cron/manager.py
@@ -9,28 +9,27 @@ import app.cron.job_fetch_new_proxies as job_fetch_new_proxies
 def start_jobs(context, telegram_api, bot_api):
     scheduler = BackgroundScheduler(
         {'apscheduler.job_defaults.max_instances': 5})
-    job_fetch_new_proxies.start(context, telegram_api)
     # job add message to channel
     scheduler.add_job(
         lambda: job_channel_add_message.start(context, bot_api),
-        trigger=CronTrigger.from_crontab('*/15 * * * *')
+        trigger=CronTrigger.from_crontab('* */1 * * *')
     )
 
     # job edit last message of channel
     scheduler.add_job(
         lambda: job_channel_edit_message.start(context, bot_api),
-        trigger=CronTrigger.from_crontab('*/15 * * * *')
+        trigger=CronTrigger.from_crontab('*/10 * * * *')
     )
 
     # job test connection of proxy base on reports
     scheduler.add_job(
         lambda: job_connection_analize.start(context),
-        trigger=CronTrigger.from_crontab('*/2 * * * *')
+        trigger=CronTrigger.from_crontab('*/5 * * * *')
     )
 
     # job fetch new proxies from other proxy chaneels
     scheduler.add_job(
         lambda: job_fetch_new_proxies.start(context, telegram_api),
-        trigger=CronTrigger.from_crontab('* * * * *')
+        trigger=CronTrigger.from_crontab('*/15 * * * *')
     )
     scheduler.start()
