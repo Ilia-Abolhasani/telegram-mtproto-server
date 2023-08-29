83e9f0cc74334e78b2861435d4b29deaafa7a318
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-08-30 00:41:39 +0330

    rollback

--- app/controller/report_controller.py
+++ app/controller/report_controller.py
@@ -6,7 +6,17 @@ class ReportController:
         self.context = Context()
 
     def post_speed(self, agent_id, reports):
-        self.context.add_bach_speed_report(agent_id, reports)
+        try:
+            self.context.add_bach_speed_report(agent_id, reports)
+        except Exception as error:
+            print("Error:", error)
+            self.context.session.rollback()
+            raise error
 
     def post_ping(self, agent_id, reports):
-        self.context.add_bach_ping_report(agent_id, reports)
+        try:
+            self.context.add_bach_ping_report(agent_id, reports)
+        except Exception as error:
+            print("Error:", error)
+            self.context.session.rollback()
+            raise error
