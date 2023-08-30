41edb3f4c845edfdddfea24e5dd83ba84e40f62c
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-08-30 23:14:09 +0330

    try except added to job analize

--- app/cron/job_connection_analize.py
+++ app/cron/job_connection_analize.py
@@ -4,4 +4,8 @@ from app.cron import job_lock
 def start(context):
     global job_lock
     with job_lock:
-        context.proxies_connection_update()
+        try:
+            context.proxies_connection_update()
+        except Exception as error:
+            print(error)
+            context.session.rollback()
