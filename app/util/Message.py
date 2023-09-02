5a8236a669afaf37404707c1d12a9d7c91d19722
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-09-02 22:50:17 +0330

    star make to hyper link, star score changed.

--- app/util/Message.py
+++ app/util/Message.py
@@ -11,11 +11,11 @@ def padding(text, num):
 
 def create_star(ping, speed):
     num_star = 1
-    if (speed > 1000 and ping < 200):
+    if (speed > 2000 and ping < 120):
         num_star = 5
-    elif (speed > 700 and ping < 200):
+    elif (speed > 1000 and ping < 200):
         num_star = 4
-    elif (speed > 400 and ping < 300):
+    elif (speed > 500 and ping < 300):
         num_star = 3
     elif (speed > 200 and ping < 400):
         num_star = 2
@@ -32,7 +32,7 @@ def create_message(proxies, connect_num, total, channels_num):
         speed = f"<b>speed:</b> {padding(speed, 4)} MB/s"
         ping = f"<b>ping:</b> {padding(proxy.average_ping // 1, 5)} ms"
         star = create_star(proxy.average_ping, proxy.average_speed)
-        proxy_info = f"<i><a href='{url}'>📶 Connect Proxy</a> {star}\nℹ️ {speed} | {ping}</i>\n"
+        proxy_info = f"<i><a href='{url}'>📶 Connect Proxy {star}</a>\nℹ️ {speed} | {ping}</i>\n"
         message += proxy_info + "\n"
 
     message += "<b>Database Status:</b>\n"
