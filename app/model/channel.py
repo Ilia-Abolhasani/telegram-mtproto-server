f8db749276710ff26730d7aaf948227e2ae6b475
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-09-08 22:30:26 +0330

    channel model changed.

--- app/model/channel.py
+++ app/model/channel.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy import Column, Integer, Boolean, String, BigInteger
 from .base import Base
 
 
 class Channel(Base):
     __tablename__ = 'channel'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=True)
     last_id = Column(BigInteger, server_default=None, nullable=True)
+    is_public = Column(Boolean, server_default='1', nullable=False)
+    chat_id = Column(BigInteger, nullable=True)
